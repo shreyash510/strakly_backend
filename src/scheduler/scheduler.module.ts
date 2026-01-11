97bf580b25ffa2fe79ce0c46b9dcc9d137b43bc8
shreyesh k <shreyeshk@iconnectsolutions.com>
2026-01-11 16:46:03 +0530

    feat: Integrate OpenAI for activity simulation and add API testing file

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
+import { ActivitySchedulerService } from './activity-scheduler.service';
 import { SchedulerController } from './scheduler.controller';
 import { StreaksModule } from '../streaks/streaks.module';
 import { ChallengesModule } from '../challenges/challenges.module';
+import { DatabaseModule } from '../database/database.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
-  imports: [StreaksModule, ChallengesModule],
+  imports: [StreaksModule, ChallengesModule, DatabaseModule, PostsModule],
   controllers: [SchedulerController],
-  providers: [SchedulerService],
+  providers: [SchedulerService, ActivitySchedulerService],
+  exports: [ActivitySchedulerService],
 })
 export class SchedulerModule {}
